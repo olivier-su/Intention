cfeb789ae82a9e316a69b374423ccf3c6003353c
olivier su <olivier_su_@hotmail.com>
2022-08-26 00:46:29 -0400

    restructured server to use app instead

--- server/server.js
+++ server/server.js
@@ -6,25 +6,28 @@ const { getFood } = require("./routes/food/foodHandlers");
 
 const PORT = 4000;
 
-express()
-  .use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  })
-  .use(morgan("tiny"))
-  .use(express.static("./server/assets"))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use("/", express.static(__dirname + "/"))
+const app = express();
 
-  //Endpoints
-  .get("/api/food", getFood)
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+app.use(morgan("tiny"));
+
+app.use(express.static("./server/assets"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(__dirname + "/"));
+
+//Endpoints
+app.get("/api/food", getFood);
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
